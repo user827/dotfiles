b05a0d6d617412b7f64f3b7708490b134fff4767
user827 <user827@users.noreply.github.com>
2023-12-31 11:09:46 +0200

    Switch pulse to pipewire

--- modules/pulse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-install_module() {
-  confpath=$modbase/pulse
-  [ -d "$L_GEN_DIR/xdg_config/pulse" ] || mkdir "$L_GEN_DIR/xdg_config/pulse"
-  sed "s,SED_THIS_DIR,$L_XDG_CONFIG/pulse,g" "$confpath/default.pa.gen" > "$L_GEN_DIR/xdg_config/pulse/default.pa"
-  [ -f "$L_GEN_DIR/xdg_config"/pulse/local.pa ] || touch "$L_XDG_CONFIG"/pulse/local.pa
-}
