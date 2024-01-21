adaef356cbe456da1a32cf4908a5b51078acd5a8
user827 <user827@users.noreply.github.com>
2024-01-21 12:57:39 +0200

    Admin mail fixup

--- dot_files/bin/admin_notify.sh
+++ dot_files/bin/admin_notify.sh
@@ -11,7 +11,7 @@ from=$(printf '%s\n' "$content" | grep '^From:' | sed 's/^From: //' | head -1)
 hostname=$(hostname)
 if printf '%s\n' "$from" | grep -q "^.*Mail Delivery System <MAILER-DAEMON@$hostname\\.localdomain>$"; then
   :
-elif printf '%s\n' "$from" | grep -q "^.*root <root@$hostname\\.localdomain>$" && printf '%s\n' "$subject" | grep -q '^sec: '; then
+elif printf '%s\n' "$from" | grep -q "\\<root@$hostname\\.localdomain\\>$" && printf '%s\n' "$subject" | grep -q '^sec: '; then
   summary=$(printf '%s\n' "$subject" | sed 's/^sec: //')
   echo "Subject: Critical event: $summary" | sendmail "$@"
 else
