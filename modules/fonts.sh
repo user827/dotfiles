408ff37379adef1709d364ee2c415b7a4c8d9af5
user827 <user827@users.noreply.github.com>
2026-02-23 14:09:40 +0200

    up

--- modules/fonts.sh
+++ modules/fonts.sh
@@ -6,14 +6,22 @@ install_module() {
     *'SauceCodePro Nerd Font Mono'*)
       ;;
     *)
-      log warn 'Terminal font SauceCodePro Nerd Font Mono is missing'
+      if command -v pacman > /dev/null; then
+        sudo pacman -S ttf-sourcecodepro-nerd
+      else
+        log warn 'Terminal font SauceCodePro Nerd Font Mono is missing'
+      fi
       ;;
   esac
   case "$fc_list" in
     *'Noto Color Emoji'*)
       ;;
     *)
-      log warn 'emoji colorfont missing'
+      if command -v pacman > /dev/null; then
+        sudo pacman -S noto-fonts-emoji
+      else
+        log warn 'Terminal font Noto Font Emoji is missing'
+      fi
       ;;
   esac
 }
